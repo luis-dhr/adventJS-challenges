1c86c82fca10ed6d045d93f97ed29c47d20a6dc7
luis-dhr <ldwhr01@gmail.com>
2022-12-23 13:40:58 -0600

    The best path

--- 14-the-best-path/get-optimal-path.js
+++ 14-the-best-path/get-optimal-path.js
@@ -1,4 +1,4 @@
-function getOptimalPath (path = []) {
+export function getOptimalPath (path = []) {
   const optimalPath = path.reduceRight((prev, curr) => {
     const newRow = curr.map((val, idx) => {
       return val + Math.min(prev[idx], prev[idx + 1])
@@ -9,13 +9,6 @@ function getOptimalPath (path = []) {
   return optimalPath[0]
 }
 
-getOptimalPath([[0], [2, 3]]) // 2
-// 0 -> 2
-
-getOptimalPath([[0], [3, 4], [9, 8, 1]]) // 5
-// 0 -> 4 -> 1
-
-getOptimalPath([[1], [1, 5], [7, 5, 8], [9, 4, 1, 3]]) // 8
 /*
     0
    / \
