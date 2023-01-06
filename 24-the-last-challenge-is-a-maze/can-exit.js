4c1022c3692605ac016efff9b77faea3dd009e2a
luis-dhr <ldwhr01@gmail.com>
2023-01-05 18:41:47 -0600

    The last challenge is a maze

--- 24-the-last-challenge-is-a-maze/can-exit.js
+++ 24-the-last-challenge-is-a-maze/can-exit.js
@@ -1,9 +1,41 @@
 export function canExit (maze) {
+  const startRow = maze.findIndex(row => row.includes('S'))
+  const start = { row: startRow, col: maze[startRow].indexOf('S') }
+
+  const endRow = maze.findIndex(row => row.includes('E'))
+  const end = { row: endRow, col: maze[endRow].indexOf('E') }
+
+  const visited = new Set()
+  const queue = [start]
+
+  while (queue.length > 0) {
+    const { row, col } = queue.shift()
+
+    if (visited.has(`${row}-${col}`)) continue
+
+    visited.add(`${row}-${col}`)
+
+    if (row === end.row && col === end.col) return true
+
+    const right = maze[row][col + 1]
+    if (right && right !== 'W') queue.push({ row, col: col + 1 })
+
+    const left = maze[row][col - 1]
+    if (left && left !== 'W') queue.push({ row, col: col - 1 })
+
+    const up = maze[row - 1]
+    if (up && up[col] !== 'W') queue.push({ row: row - 1, col })
+
+    const down = maze[row + 1]
+    if (down && down[col] !== 'W') queue.push({ row: row + 1, col })
+  }
+
   return false
 }
 
 // ¡Ha llegado el día!
-// Hoy vamos a repartir los regalos… pero los almacenes son un labertinto y los elfos están perdidos.
+// Hoy vamos a repartir los regalos…
+// pero los almacenes son un labertinto y los elfos están perdidos.
 
 // Indielfo Jones quiere escribir un programa que al recibir una matriz,
 // sepa si puede salir o no del laberinto rápidamente desde su entrada a la salida.
